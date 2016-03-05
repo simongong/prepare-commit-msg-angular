9a04f25dac59987ccba2b6296492d788fe4a827f
simongong <simon.gong64@gmail.com>
2016-03-05 23:59:42 +0800

    feat(lib): add `-t` option to support customizing message template

---
+++ default.tpl
@@ -0,0 +1,25 @@
+# <type>(<scope>): <subject>
+# <BLANK LINE>
+# <body>
+# <BLANK LINE>
+# <footer>
+#---
+# TYPE must be one of the following:
+# - feat: A new feature (will appear in CHANGELOG)
+# - fix: A bug fix (will appear in CHANGELOG)
+# - docs: Documentation only changes
+# - style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+# - refactor: A code change that neither fixes a bug nor adds a feature
+# - perf: A code change that improves performance (will appear in CHANGELOG)
+# - test: Adding missing tests
+# - chore: Changes to the build process or auxiliary tools and libraries such as documentation generation
+#
+# SCOPE could be anything specifying place of the commit change.
+# For example $location, $browser, $compile, $rootScope, ngHref, ngClick, ngView, etc...
+#
+# SUBJECT contains succinct description of the change:
+# - use the imperative, present tense: "change" not "changed" nor "changes"
+# - do not capitalize first letter
+# - no dot (.) at the end
+#---
+# https://github.com/ajoslin/conventional-changelog/blob/master/conventions/angular.md
--- index.js
+++ index.js
@@ -4,38 +4,32 @@
 
 var fs = require('fs');
 var execSync = require('child_process').execSync;
+var program = require('commander');
+var clc = require('cli-color');
+var consoleError = clc.red.bold;
 
 var commitMsgFile = process.argv[2] || './.git/COMMIT_EDITMSG';
 var isNewCommit = (process.argv[3] === 'undefined');
 
 var maxCharsLine = '#⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴⎴';
-var scissorsLine = '# ------------------------ >8 ------------------------';
-var doc = `${scissorsLine}
-# <type>(<scope>): <subject>
-# <BLANK LINE>
-# <body>
-# <BLANK LINE>
-# <footer>
-#---
-# TYPE must be one of the following:
-# - feat: A new feature (will appear in CHANGELOG)
-# - fix: A bug fix (will appear in CHANGELOG)
-# - docs: Documentation only changes
-# - style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
-# - refactor: A code change that neither fixes a bug nor adds a feature
-# - perf: A code change that improves performance (will appear in CHANGELOG)
-# - test: Adding missing tests
-# - chore: Changes to the build process or auxiliary tools and libraries such as documentation generation
-#
-# SCOPE could be anything specifying place of the commit change.
-# For example $location, $browser, $compile, $rootScope, ngHref, ngClick, ngView, etc...
-#
-# SUBJECT contains succinct description of the change:
-# - use the imperative, present tense: "change" not "changed" nor "changes"
-# - do not capitalize first letter
-# - no dot (.) at the end
-#---
-# https://github.com/ajoslin/conventional-changelog/blob/master/conventions/angular.md
+var scissorsLine = '# ---------- Message Formatting Convention ----------';
+
+program
+  .option('-t, --template [file path]', 'Set file location of template for git commit message', '')
+  .parse(process.argv);
+var msgTplFile = !program.template ? './default.tpl' : program.template;
+try {
+  fs.accessSync(msgTplFile, fs.R_OK)
+} catch (err) {
+  console.error(consoleError('Invalid path for template file: ' + msgTplFile));
+  process.exit(1);
+}
+
+var msgTpl = fs.readFileSync(msgTplFile).toString();
+
+var doc = `
+${scissorsLine}
+${msgTpl}
 `;
 
 function getPrefilledMessage() {
